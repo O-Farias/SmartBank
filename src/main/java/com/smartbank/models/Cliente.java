d8587752657a6dc475b0ca1b7b26989657ea6e40
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-13 21:28:20 +0000

    cria classe `Cliente` para representar os dados dos clientes.

---
+++ src/main/java/com/smartbank/models/Cliente.java
@@ -0,0 +1,19 @@
+package com.smartbank.models;
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+
+    public Cliente(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+}
