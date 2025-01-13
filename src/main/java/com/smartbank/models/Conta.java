f5a8a780527009111a0a93573cded46f6dd28d5c
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-13 21:29:17 +0000

    cria classe `Conta` com funcionalidades de depósito e saque.

---
+++ src/main/java/com/smartbank/models/Conta.java
@@ -0,0 +1,41 @@
+package com.smartbank.models;
+
+public class Conta {
+    private String numero;
+    private String titular;
+    private double saldo;
+
+    public Conta(String numero, String titular, double saldoInicial) {
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = saldoInicial;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+        } else {
+            System.out.println("Valor inválido para depósito!");
+        }
+    }
+
+    public void sacar(double valor) {
+        if (valor > 0 && saldo >= valor) {
+            saldo -= valor;
+        } else {
+            System.out.println("Saldo insuficiente ou valor inválido!");
+        }
+    }
+}
