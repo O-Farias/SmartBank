f1c789697148a7cb689b9e16a6022ce5a44f2779
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-13 21:42:10 +0000

    Atualiza Main para testar transferências entre contas

--- src/main/java/com/smartbank/app/Main.java
+++ src/main/java/com/smartbank/app/Main.java
@@ -5,16 +5,31 @@ import com.smartbank.models.Conta;
 
 public class Main {
     public static void main(String[] args) {
-        Cliente cliente = new Cliente("Mateus", "123.456.789-00");
-        Conta conta = new Conta("001", cliente.getNome(), 1000);
+        
+        Cliente cliente1 = new Cliente("Mateus", "123.456.789-00");
+        Conta conta1 = new Conta("001", cliente1.getNome(), 1000);
 
-        System.out.println("Cliente: " + cliente.getNome());
-        System.out.println("Saldo inicial: R$ " + conta.getSaldo());
+        Cliente cliente2 = new Cliente("João", "987.654.321-00");
+        Conta conta2 = new Conta("002", cliente2.getNome(), 500);
 
-        conta.depositar(500);
-        System.out.println("Saldo após depósito: R$ " + conta.getSaldo());
+        
+        System.out.println("Saldo inicial conta1 (" + cliente1.getNome() + "): R$ " + conta1.getSaldo());
+        System.out.println("Saldo inicial conta2 (" + cliente2.getNome() + "): R$ " + conta2.getSaldo());
 
-        conta.sacar(200);
-        System.out.println("Saldo após saque: R$ " + conta.getSaldo());
+        
+        System.out.println("\nRealizando transferência de R$ 300 da conta1 para a conta2...");
+        conta1.transferir(conta2, 300);
+
+        
+        System.out.println("\nSaldo após transferência conta1: R$ " + conta1.getSaldo());
+        System.out.println("Saldo após transferência conta2: R$ " + conta2.getSaldo());
+
+        
+        System.out.println("\nTentando transferir R$ 1000 da conta1 para a conta2...");
+        conta1.transferir(conta2, 1000);
+
+        
+        System.out.println("\nSaldo final conta1: R$ " + conta1.getSaldo());
+        System.out.println("Saldo final conta2: R$ " + conta2.getSaldo());
     }
 }
